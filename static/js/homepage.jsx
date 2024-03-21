2565eb4bf93c51f8369211d2cf425dac79724d73
Emily <emily.j.langley@gmail.com>
2024-03-21 12:21:41 -0700

    updated card skill

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,18 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+    <React.Fragment>
+      <h1>Welcome to Our Website!</h1>
+      <div>
+        <p>Welcome to our website. We're excited to have you here!</p>
+      </div>
+      <div>
+        <p>Click <a href="/cards">here</a> to go to the Cards Page.</p>
+      </div>  
+      <img src="/static/img/balloonicorn.jpg" alt="Balloonicorn" style={{ width: '100%', maxWidth: '600px', margin: '20px auto', display: 'block' }} />
+    </React.Fragment>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
